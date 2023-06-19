d83cba566185f91f72baedd5ce673c09664b2113
benudall98 <bu12@outlook.com>
2023-06-19 12:04:25 +0100

    Implement DetailsComponent view

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -4,12 +4,14 @@ import { ProductsListComponent } from './products-list/products-list.component';
 import { AddProductComponent } from './add-product/add-product.component'; 
 import { EditProductComponent } from './edit-product/edit-product.component'; 
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 const routes: Routes = [
   // { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: '', component: HomeComponent},
   { path: 'products', component: ProductsListComponent }, 
   { path: 'products/new', component: AddProductComponent }, // 
-  { path: 'products/edit/:id', component: EditProductComponent }];
+  { path: 'products/edit/:id', component: EditProductComponent }, 
+  {path: 'details/:id', component: DetailsComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
